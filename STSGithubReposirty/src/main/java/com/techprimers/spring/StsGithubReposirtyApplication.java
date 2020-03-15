a1c4e4848fa2a231bff14d9a0beec543c7e8168b
sumitpaw <sumitpaw@SUMITPAW01.corp.amdocs.com>
2020-03-15 10:52:13 +0530

    First Commit.

---
+++ STSGithubReposirty/src/main/java/com/techprimers/spring/StsGithubReposirtyApplication.java
@@ -0,0 +1,13 @@
+package com.techprimers.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StsGithubReposirtyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StsGithubReposirtyApplication.class, args);
+	}
+
+}
